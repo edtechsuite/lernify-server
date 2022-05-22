4e937aa6f56ee1631829cf1e3fd45d750675222f
Dmytro Borchuk <frunk.lern@gmail.com>
2022-05-22 23:04:08 +0300

    Fixed \n in the env for heroku

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,3 +1,3 @@
 export const PROJECT_ID = process.env.PROJECT_ID
 export const CLIENT_EMAIL = process.env.CLIENT_EMAIL
-export const PRIVATE_KEY = process.env.PRIVATE_KEY
+export const PRIVATE_KEY = (process.env.PRIVATE_KEY || '').replace(/\\n/g, '\n')
